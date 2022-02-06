f57c02855e0b422e1566b95964b03a2094b17b70
plug47 <oarmoa44@gmail.com>
2022-02-05 22:09:05 -0300

    Section 17 API & Backend Authorization FINISHED

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log('This is server.js')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+//Connect to DB
+connectDB()
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+//Routes
+app.use('/api/users', require('./routes/UserRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
